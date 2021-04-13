f7f9a76c1309cc2ba8dd07ae948c571ef02006fc
chenlei <ruyueshi@qq.com>
2021-04-13 22:14:30 +0800

    Add lonest_nonrepeating_substring.h

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include "array/alibaba_2013.h"
-#include "array/min_distance.h"
 #include "array/get_min_value_from_array.h"
-#include "graph/shorted_bridge_time.h"
+#include "array/min_distance.h"
 #include "graph/graph_djkstra.h"
+#include "graph/shorted_bridge_time.h"
 #include "linked_list/reverse_list.h"
+#include "other/cpu_double_core.h"
 #include "other/find_water_king.h"
 #include "other/find_special_value.h"
-#include "other/cpu_double_core.h"
 #include "other/huiwen.h"
+#include "other/lonest_nonrepeating_substring.h"
 #include "recursion/factorization.h"
 #include "string/move_star_forward.h"
 
@@ -23,7 +24,8 @@ int main() {
 //    FindWaterKingTest();
 //    BitsetTest();
 //    FindSpecialValueTest();
-    MinRunTimeTest();
+//    MinRunTimeTest();
 //    SolveTest();
+    LongestNonRepeatingSubstringTest();
     return 0;
 }
---
+++ other/lonest_nonrepeating_substring.h
@@ -0,0 +1,37 @@
+#ifndef ALGORITHM_DESIGN_ANALYSIS_LONEST_NONREPEATING_SUBSTRING_H
+#define ALGORITHM_DESIGN_ANALYSIS_LONEST_NONREPEATING_SUBSTRING_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::string LongestNonRepeatingSubstring(const std::string &src) {
+    std::vector<int> bits(26, -1);
+    // longest_substring 记录最长不重复子串的信息，数组的两个元素分别表示最长不重复子串的起始位置和长度
+    int longest_substring[2] = {0, 0};
+    int current_substring[2] = {0, 0};
+    for (int i = 0; i < src.length(); i++) {
+        int index = src[i] - 'a';
+        if (bits[index] < 0) {
+            current_substring[1]++;
+            if (current_substring[1] > longest_substring[1])
+                std::copy(current_substring, current_substring + 2, longest_substring);
+        } else {// 一旦bits[index] >= 0，说明字符char(index)在之前出现过，更新当前子串
+            current_substring[0] = bits[index] + 1;
+            current_substring[1] = i - index;
+        }
+        bits[index] = i;
+    }
+    return src.substr(longest_substring[0], longest_substring[1]);
+}
+
+void LongestNonRepeatingSubstringTest() {
+    std::string test_cases[] = {"absd", "abba", "abdffd", "abcdaefghij", "abcddefg", "abcdabcghysaind"};
+    for (const auto &s: test_cases) {
+        auto res = LongestNonRepeatingSubstring(s);
+        std::cout << "Input: " << s << ", Output: " <<  res << ", " << res.length() << std::endl;
+    }
+}
+
+#endif //ALGORITHM_DESIGN_ANALYSIS_LONEST_NONREPEATING_SUBSTRING_H
